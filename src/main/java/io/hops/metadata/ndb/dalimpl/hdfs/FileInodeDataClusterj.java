25bc21fe87c5272cdfc53a05da4b63ae6d3a2322
Salman Niazi <salman@sics.se>
2016-04-01 17:08:28 +0200

    read db file working. committing partial changes to work from home

--- src/main/java/io/hops/metadata/ndb/dalimpl/hdfs/FileInodeDataClusterj.java
+++ src/main/java/io/hops/metadata/ndb/dalimpl/hdfs/FileInodeDataClusterj.java
@@ -13,41 +13,56 @@ import org.apache.log4j.Logger;
  * Created by salman on 3/10/16.
  */
 public class FileInodeDataClusterj
-        implements TablesDef.FileInodeData, FileInodeDataDataAccess<FileInodeData>
-{
-    static final Logger LOG = Logger.getLogger(FileInodeDataClusterj.class);
-    private ClusterjConnector connector = ClusterjConnector.getInstance();
+        implements TablesDef.FileInodeData, FileInodeDataDataAccess<FileInodeData> {
+  static final Logger LOG = Logger.getLogger(FileInodeDataClusterj.class);
+  private ClusterjConnector connector = ClusterjConnector.getInstance();
 
 
-    @PersistenceCapable(table = TABLE_NAME)
-    public interface FileInodeDataDTO {
+  @PersistenceCapable(table = TABLE_NAME)
+  public interface FileInodeDataDTO {
 
-        @PrimaryKey
-        @Column(name = ID)
-        int getInodeId();
-        void setInodeId(int inodeId);
+    @PrimaryKey
+    @Column(name = ID)
+    int getInodeId();
 
-        @Column(name = DATA)
-        byte[] getData();
-        void setData(byte[] data);
+    void setInodeId(int inodeId);
+
+    @Column(name = DATA)
+    byte[] getData();
+
+    void setData(byte[] data);
+  }
+
+
+  @Override
+  public void add(FileInodeData fileInodeData) throws StorageException {
+    final HopsSession session = connector.obtainSession();
+
+    FileInodeDataDTO dto = session.newInstance(FileInodeDataClusterj.FileInodeDataDTO.class);
+    dto.setInodeId(fileInodeData.getInodeId());
+    dto.setData(fileInodeData.getInodeData());
+    session.savePersistent(dto);
+    session.release(dto);
+  }
+
+  @Override
+  public void delete(FileInodeData fileInodeData) throws StorageException {
+    final HopsSession session = connector.obtainSession();
+    session.deletePersistent(FileInodeDataClusterj.FileInodeDataDTO.class, fileInodeData.getInodeData());
+  }
+
+  @Override
+  public FileInodeData get(int inodeId) throws StorageException {
+    final HopsSession session = connector.obtainSession();
+    FileInodeDataDTO dataDto = session.find(FileInodeDataDTO.class, inodeId);
+    if (dataDto != null) {
+      byte[] data = new byte[dataDto.getData().length];
+      System.arraycopy(dataDto.getData(),0,data,0,data.length);
+      FileInodeData fileData = new FileInodeData(inodeId, data);
+      session.release(dataDto);
+      return fileData;
     }
 
-
-
-    @Override
-    public void add(FileInodeData fileInodeData) throws StorageException {
-        final HopsSession session = connector.obtainSession();
-
-        FileInodeDataDTO dto =  session.newInstance(FileInodeDataClusterj.FileInodeDataDTO.class);
-        dto.setInodeId(fileInodeData.getInodeId());
-        dto.setData(fileInodeData.getInodeData());
-        session.savePersistent(dto);
-        session.release(dto);
-    }
-
-    @Override
-    public void delete(FileInodeData fileInodeData) throws StorageException {
-        final HopsSession session = connector.obtainSession();
-        session.deletePersistent(FileInodeDataClusterj.FileInodeDataDTO.class, fileInodeData.getInodeData());
-    }
+    return null;
+  }
 }
