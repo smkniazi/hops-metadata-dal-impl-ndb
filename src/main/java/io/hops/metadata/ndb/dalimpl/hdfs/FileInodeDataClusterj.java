a75d72fa4a1072fd02ac84e58fe90a8368b40364
Salman Niazi <salman@sics.se>
2016-04-05 18:29:41 +0200

    work from home

--- src/main/java/io/hops/metadata/ndb/dalimpl/hdfs/FileInodeDataClusterj.java
+++ src/main/java/io/hops/metadata/ndb/dalimpl/hdfs/FileInodeDataClusterj.java
@@ -6,8 +6,12 @@ import io.hops.metadata.hdfs.TablesDef;
 import io.hops.metadata.hdfs.dal.FileInodeDataDataAccess;
 import io.hops.metadata.hdfs.entity.FileInodeData;
 import io.hops.metadata.ndb.ClusterjConnector;
+import io.hops.metadata.ndb.wrapper.HopsQuery;
+import io.hops.metadata.ndb.wrapper.HopsQueryBuilder;
 import io.hops.metadata.ndb.wrapper.HopsSession;
 import org.apache.log4j.Logger;
+
+import java.util.List;
 
 /**
  * Created by salman on 3/10/16.
@@ -48,7 +52,7 @@ public class FileInodeDataClusterj
   @Override
   public void delete(FileInodeData fileInodeData) throws StorageException {
     final HopsSession session = connector.obtainSession();
-    session.deletePersistent(FileInodeDataClusterj.FileInodeDataDTO.class, fileInodeData.getInodeData());
+    session.deletePersistent(FileInodeDataClusterj.FileInodeDataDTO.class, fileInodeData.getInodeId());
   }
 
   @Override
@@ -64,5 +68,14 @@ public class FileInodeDataClusterj
     }
 
     return null;
+  }
+
+  @Override
+  public int count() throws StorageException {
+    HopsSession session = connector.obtainSession();
+    HopsQueryBuilder qb = session.getQueryBuilder();
+    HopsQuery<FileInodeDataDTO> query =
+        session.createQuery(qb.createQueryDefinition(FileInodeDataDTO.class));
+    return query.getResultList().size();
   }
 }
